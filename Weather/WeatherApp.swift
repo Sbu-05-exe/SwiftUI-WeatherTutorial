e813016d30a7e4f2606cf7c07ed259b7fa986b7f
Sibusiso Dlamini <g20d9300@campus.ru.ac.za>
2023-11-19 01:09:55 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sibusiso Dlamini on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
